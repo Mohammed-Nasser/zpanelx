b5fad2548c01d77a5a830f4b657e51a5c159a338
Chris Stokes <stokes84@gmail.com>
2013-03-29 19:48:59 -0700

    Update progbarbandwidth.class.php

--- dryden/ui/tpl/progbarbandwidth.class.php
+++ dryden/ui/tpl/progbarbandwidth.class.php
@@ -18,28 +18,12 @@ class ui_tpl_progbarbandwidth {
         $bandwidthquota = $currentuser['bandwidthquota'];
         $bandwidth = ctrl_users::GetQuotaUsages('bandwidth', $currentuser['userid']);
         if ($bandwidthquota == 0) {
-<<<<<<< HEAD
-<<<<<<< HEAD
             return "<img src=\"etc/lib/pChart2/zpanel/zProgress.php?percent=0\"/>";
         } else {
             if (fs_director::CheckForEmptyValue($bandwidth))
                 $bandwidth = 0;
             $percent = round(($bandwidth / $bandwidthquota) * 100, 0);
             return "<img src=\"etc/lib/pChart2/zpanel/zProgress.php?percent=" . $percent . "\"/>";
-=======
-=======
->>>>>>> ee7d29f... Enable Diskspace=0 and Bandwidth=0 as "Unlimited"
-            return ''; // no quota, it is desactivated
-        }
-        else {
-            if (fs_director::CheckForEmptyValue($bandwidth))
-                $bandwidth = 0;
-            $percent = round(($bandwidth / $bandwidthquota) * 100, 0);
-            return '<img src="etc/lib/pChart2/zpanel/zProgress.php?percent=$percent"/>';
-<<<<<<< HEAD
->>>>>>> ee7d29f... Enable Diskspace=0 and Bandwidth=0 as "Unlimited"
-=======
->>>>>>> ee7d29f... Enable Diskspace=0 and Bandwidth=0 as "Unlimited"
         }
     }
 
