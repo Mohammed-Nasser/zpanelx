cd804d4776ccdfefffb9eaa3ea86acfa75bfbe89
Chris Stokes <stokes84@gmail.com>
2013-03-29 22:09:47 -0700

    updated for bootstrep

--- dryden/ui/tpl/notice.class.php
+++ dryden/ui/tpl/notice.class.php
@@ -13,12 +13,14 @@
 class ui_tpl_notice {
 
     public static function Template() {
+        global $controller;
+        if (!$controller->GetControllerRequest('URL', 'module')) {
         $user_array = ctrl_users::GetUserDetail();
         global $zdbh;
         $result = $zdbh->query("SELECT ac_notice_tx FROM x_accounts WHERE ac_id_pk = " . $user_array['resellerid'] . "")->Fetch();
         if ($result) {
             if ($result['ac_notice_tx'] <> "")
-                return "<div class=\"znotice\" id=\"znotice\"><a href=\"#\" class=\"znotice\" id=\"znotice_a\" border=\"0\"></a>" . runtime_xss::xssClean($result['ac_notice_tx']) . "</div>";
+                return "<div class=\"clearfix\"></div><div id=\"client_notice\" class=\"client-notice alert alert-info fade in\" data-alert=\"alert\"><button type=\"button\" id=\"notice-close\" class=\"close\" data-dismiss=\"alert\">&times;</button>" . runtime_xss::xssClean($result['ac_notice_tx']) . "</div>";
             return false;
         } else {
             return false;
@@ -26,5 +28,5 @@ class ui_tpl_notice {
     }
 
 }
-
+}
 ?>
